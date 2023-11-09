cc61a10623099d1037a3a3bcee43f97019390720
Sonali Misra <sonalimisra2001@gmail.com>
2023-11-09 12:08:45 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def jampandu(request):
+    return HttpResponse('hello my dear jampandu u allright')
+def jigelrani(request):
+    return HttpResponse<h1>('have a lovely day sweety')</h1>
